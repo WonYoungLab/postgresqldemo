93999d690ad49cc90de5abdaa3f714655a155229
wysong <wysong@e3ps.com>
2023-04-05 12:31:17 +0900

    id : #1016
    Signed-off-by: wysong <wysong@e3ps.com>

--- src/main/java/com/postgresql/postgresqldemo/TestController.java
+++ src/main/java/com/postgresql/postgresqldemo/TestController.java
@@ -5,6 +5,16 @@ public class TestController {
     public static String getName() {
         String name = "";
         System.out.println(name);
+        System.out.println(name);
+        System.out.println(name);
         return name;
+    }
+
+    public static String getUserName() {
+        String userName = "";
+        System.out.println(userName);
+        System.out.println(userName);
+        System.out.println(userName);
+        return userName;
     }
 }
